a28db6bdd7a60fec0e11959b6b3da10bad11e0e4
Sahbi Belgacem <sahbi21@gmail.com>
2020-06-02 13:42:15 +0200

    adds request status code checking

--- .circleci/updatedns.sh
+++ .circleci/updatedns.sh
@@ -5,7 +5,7 @@ set -e
 TOKEN=""  # The API v2 OAuth token
 ACCOUNT_ID=""        # Replace with your account ID
 ZONE_ID=""  # The zone ID is the name of the zone (or domain)
-RECORD_ID=""       # Replace with the Record ID
+RECORD_ID="" 
 APPUIO_CNAME="cname.appuioapp.ch"
 PROJECT_NAME=""
 
@@ -15,17 +15,39 @@ if [ -z "$TOKEN" ] || [ -z "$ACCOUNT_ID" ] || [ -z "$ZONE_ID" ] || [ -z "$RECORD
   exit 1
 fi
 
-
-# Create a new record
-curl --request POST "https://api.dnsimple.com/v2/$ACCOUNT_ID/zones/$ZONE_ID/records" \
---header "Authorization: Bearer $TOKEN" \
---header 'Content-Type: application/json' \
---header 'Accept: application/json' \
---data @- <<END; 
+function postData() 
+{
+	cat <<EOF
 {
 	"name": "$PROJECT_NAME",
 	"type": "CNAME",
 	"content": "$APPUIO_CNAME",
 	"ttl": 600
 }
-END
+EOF
+}
+
+# Create a new record
+status_code=$(curl --write-out %{http_code} --silent --output /dev/null --request POST "https://api.dnsimple.com/v2/$ACCOUNT_ID/zones/$ZONE_ID/records" \
+--header "Authorization: Bearer $TOKEN" \
+--header 'Content-Type: application/json' \
+--header 'Accept: application/json' \
+--data "$(postData)")
+case "$status_code" in
+	"201")
+		echo "New record was successfully added to your domain";
+		exit 0;
+		;;
+	"400")
+		echo "ERROR: A required parameter or the request is invalid";
+		exit 1;
+		;;
+	"401")
+		echo "ERROR: Unauthenticated. Please make sure to add the required keys";
+		exit 1;
+		;;
+	"500" | "502" | "503" | "504")
+		echo "Uh oh, something went wrong on DNSimple’s end";
+		exit 1;
+		;;		
+esac
